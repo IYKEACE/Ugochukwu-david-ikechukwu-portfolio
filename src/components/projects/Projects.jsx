7e8d0d86fc2c602493867343556406f81c6f0b8b
iykeace <ikechukwuu338@gmail.com>
2025-09-05 12:46:08 +0400

    added a new project

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -8,18 +8,18 @@ const Projects = () => {
         <h2>Projects</h2>
         <div className={styles.projectContent}>
           <div className={styles.projectCard}>
-            <img src="/estate images.png" alt="project one" />
+            <img src="/age calcule.png" alt="project one" />
             <h3>Project One</h3>
             <div className={styles.buttons}>
               <a
-                href="https://github.com/IYKEACE/Estate-landing-page"
+                href="https://iykeace.github.io/codealpha_agecalculator/"
                 target="_blank"
                 className={styles.btns}
               >
                 Github
               </a>
               <a
-                href="https://iykeace.github.io/Estate-landing-page/"
+                href="https://codealpha-agecalculator-6y2p6ez3n.vercel.app/"
                 target="_blank"
                 className={styles.btns}
               >
@@ -28,7 +28,7 @@ const Projects = () => {
             </div>
           </div>
           <div className={styles.projectCard}>
-            <img src="/srudent-img.png" alt="project one" />
+            <img src="/srudent-img.png" alt="project two" />
             <h3>Project Two</h3>
             <div className={styles.buttons}>
               <a
@@ -48,7 +48,7 @@ const Projects = () => {
             </div>
           </div>
           <div className={styles.projectCard}>
-            <img src="/portfolio bspecial.png" alt="project one" />
+            <img src="/portfolio bspecial.png" alt="project three" />
             <h3>Project Three</h3>
             <div className={styles.buttons}>
               <a
